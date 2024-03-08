29fbd1776661c4129bf26be9308f55c6b4c88edb
Zhadnaya <linenurt2@mail.ru>
2024-03-08 14:03:13 +0300

    Добавил модальное окно для заявок

--- scripts/main.js
+++ scripts/main.js
@@ -7,14 +7,24 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-  document.getElementById('my-modal').addEventListener('click', function(event) {
+/*Модальное окно контактов*/
+document.getElementById('contact-modal').addEventListener('click', function(event) {
     if (event.target.classList.contains('close')) {
       this.style.display = 'none';
     }
-  });
+});
 
-  document.getElementById('open-modal').addEventListener('click', function() {
-    document.getElementById('my-modal').style.display = 'block';
-  });
-  
-  
+document.getElementById('open-contact-modal').addEventListener('click', function() {
+document.getElementById('contact-modal').style.display = 'block';
+});
+
+/*модальное окно заявок*/
+document.getElementById('message-modal').addEventListener('click', function(event) {
+    if (event.target.classList.contains('close')) {
+      this.style.display = 'none';
+    }
+});
+
+document.getElementById('open-message-modal').addEventListener('click', function() {
+document.getElementById('message-modal').style.display = 'block';
+});
