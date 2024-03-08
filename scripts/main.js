601359b609fbbdde28182934f5d7b6fe28f230c3
Zhadnaya <linenurt2@mail.ru>
2024-03-08 13:30:40 +0300

    Добавил модальное окно

--- scripts/main.js
+++ scripts/main.js
@@ -7,4 +7,14 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+  document.getElementById('my-modal').addEventListener('click', function(event) {
+    if (event.target.classList.contains('close')) {
+      this.style.display = 'none';
+    }
+  });
 
+  document.getElementById('open-modal').addEventListener('click', function() {
+    document.getElementById('my-modal').style.display = 'block';
+  });
+  
+  
